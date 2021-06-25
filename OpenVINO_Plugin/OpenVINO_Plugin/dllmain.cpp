eebef462d5a0fe043ff0e41019ac89aca99e0b58
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2021-06-25 00:03:14 -0700

    Update OpenVINO_Plugin folder.

--- OpenVINO_Plugin/OpenVINO_Plugin/dllmain.cpp
+++ OpenVINO_Plugin/OpenVINO_Plugin/dllmain.cpp
@@ -9,13 +9,11 @@ using namespace InferenceEngine;
 // Wrap code to prevent name-mangling issues
 extern "C" {
 
-    // The name of the current compute device
-    std::string currentDevice = "";
     // List of available compute devices
     std::vector<std::string> availableDevices;
     // An unparsed list of available compute devices
     std::string allDevices = "";
-    //
+    // The name of the input layer of Neural Network "input.1"
     std::string firstInputName;
     // The name of the output layer of Neural Network "140"
     std::string firstOutputName;
@@ -32,15 +30,33 @@ extern "C" {
     // Provides an interface for an asynchronous inference request
     InferRequest infer_request;
 
+    // A poiner to the input tensor for the model
+    MemoryBlob::Ptr minput;
+    // A poiner to the output tensor for the model
+    MemoryBlob::CPtr moutput;
+
+    // The number of color channels 
+    size_t num_channels;
+    // The number of pixels in the input image
+    size_t nPixels;
+
+    // A vector for processing the raw model output
+    std::vector<float> data_img;
+
     // Returns an unparsed list of available compute devices
     DLLExport const std::string* GetAvailableDevices() {
+        // Add all available compute devices to a single string
+        for (auto&& device : availableDevices) {
+            allDevices += device;
+            allDevices += ((device == availableDevices[availableDevices.size() - 1]) ? "" : ",");
+        }
         return &allDevices;
     }
 
     // Configure the cache directory for GPU compute devices
     void SetDeviceCache() {
         std::regex e("(GPU)(.*)");
-        // Iterate through the availabel compute devices
+        // Iterate through the available compute devices
         for (auto&& device : availableDevices) {
             // Only configure the cache directory for GPUs
             if (std::regex_match(device, e)) {
@@ -54,7 +70,7 @@ extern "C" {
         // Get information about the network input
         InputsDataMap inputInfo(network.getInputsInfo());
         firstInputName = inputInfo.begin()->first;
-        inputInfo.begin()->second->setPrecision(Precision::FP32);
+        inputInfo.begin()->second->setPrecision(Precision::U8);
 
         // Get information about the network output
         OutputsDataMap outputInfo(network.getOutputsInfo());
@@ -77,11 +93,6 @@ extern "C" {
         availableDevices = ie.GetAvailableDevices();
         // Reverse the order of the list
         std::reverse(availableDevices.begin(), availableDevices.end());
-        // Add all available compute devices to a single string
-        for (auto&& device : availableDevices) {
-            allDevices += device;
-            allDevices += ((device == availableDevices[availableDevices.size() - 1]) ? "" : ",");
-        }
         // Specify the cache directory for GPU inference
         SetDeviceCache();
     }
@@ -115,87 +126,28 @@ extern "C" {
     // Create an executable network for the target compute device
     DLLExport std::string* UploadModelToDevice(int deviceNum) {
 
-        // Get the name for the selected device index
-        currentDevice = availableDevices[deviceNum];
         // Create executable network
-        executable_network = ie.LoadNetwork(network, currentDevice);
+        executable_network = ie.LoadNetwork(network, availableDevices[deviceNum]);
         // Create an inference request object
         infer_request = executable_network.CreateInferRequest();
-        // Return the name of the current compute device
-        return &currentDevice;
-    }
-
-    // Prepare input values for the model
-    void PrepareInput() {
         
         // Get a poiner to the input tensor for the model
-        MemoryBlob::Ptr minput = as<MemoryBlob>(infer_request.GetBlob(firstInputName));
-
-        // locked memory holder should be alive all time while access to its buffer happens
-        auto ilmHolder = minput->wmap();
+        minput = as<MemoryBlob>(infer_request.GetBlob(firstInputName));
+        // Get a poiner to the ouptut tensor for the model
+        moutput = as<MemoryBlob>(infer_request.GetBlob(firstOutputName));
 
         // Get the number of color channels 
-        size_t num_channels = minput->getTensorDesc().getDims()[1];
+        num_channels = minput->getTensorDesc().getDims()[1];
         // Get the number of pixels in the input image
         size_t H = minput->getTensorDesc().getDims()[2];
         size_t W = minput->getTensorDesc().getDims()[3];
-        size_t nPixels = W * H;
+        nPixels = W * H;
 
         // Filling input tensor with image data
-        auto data = ilmHolder.as<PrecisionTrait<Precision::FP32>::value_type*>();
-
-        // Iterate over each color channel for each pixel in image
-        for (size_t pixel = 0; pixel < nPixels; pixel++) {
-            /** Iterate over all channels **/
-            for (size_t ch = 0; ch < num_channels; ++ch) {
-                // [channels stride + pixel id ] all in bytes
-                data[ch * nPixels + pixel] = texture.data[pixel * num_channels + ch];
-            }
-        }
-    }
-
-    // Process the raw output from the model
-    void ProcessOutput() {
+        data_img = std::vector<float>(nPixels * num_channels);
         
-        MemoryBlob::CPtr moutput = as<MemoryBlob>(infer_request.GetBlob(firstOutputName));
-
-        // locked memory holder should be alive all time while access to its buffer happens
-        auto lmoHolder = moutput->rmap();
-        const auto output_data = lmoHolder.as<const PrecisionTrait<Precision::FP32>::value_type*>();
-
-        // Get the number of color channels 
-        size_t num_channels = moutput->getTensorDesc().getDims()[1];
-        // Get the number of pixels in the input image
-        size_t H = moutput->getTensorDesc().getDims()[2];
-        size_t W = moutput->getTensorDesc().getDims()[3];
-        size_t nPixels = W * H;
-
-        // Create a temporary vector for processing the raw model output
-        std::vector<float> data_img(nPixels * num_channels);
-
-        // Iterate through each pixel in the model output
-        for (size_t i = 0; i < nPixels; i++) {
-
-            // Get values from the model output
-            data_img[i * num_channels] = static_cast<float>(output_data[i]);
-            data_img[i * num_channels + 1] = static_cast<float>(output_data[(i + nPixels)]);
-            data_img[i * num_channels + 2] = static_cast<float>(output_data[(i + 2 * nPixels)]);
-
-            // Clamp color values to the range [0, 255]
-            if (data_img[i * num_channels] < 0) data_img[i * num_channels] = 0;
-            if (data_img[i * num_channels] > 255) data_img[i * num_channels] = 255;
-
-            if (data_img[i * num_channels + 1] < 0) data_img[i * num_channels + 1] = 0;
-            if (data_img[i * num_channels + 1] > 255) data_img[i * num_channels + 1] = 255;
-
-            if (data_img[i * num_channels + 2] < 0) data_img[i * num_channels + 2] = 0;
-            if (data_img[i * num_channels + 2] > 255) data_img[i * num_channels + 2] = 255;
-
-            // Copy the processed output to the OpenCV Mat
-            texture.data[i * num_channels] = data_img[i * num_channels];
-            texture.data[i * num_channels + 1] = data_img[i * num_channels + 1];
-            texture.data[i * num_channels + 2] = data_img[i * num_channels + 2];
-        }
+        // Return the name of the current compute device
+        return &availableDevices[deviceNum];;
     }
        
     // Perform inference with the provided texture data
@@ -206,16 +158,46 @@ extern "C" {
         // Remove the alpha channel
         cv::cvtColor(texture, texture, cv::COLOR_RGBA2RGB);
         
-        PrepareInput();
+        // locked memory holder should be alive all time while access to its buffer happens
+        LockedMemory<void> ilmHolder = minput->wmap();
+
+        // Filling input tensor with image data
+        auto input_data = ilmHolder.as<PrecisionTrait<Precision::U8>::value_type*>();
+
+        // Iterate over each pixel in image
+        for (size_t p = 0; p < nPixels; p++) {
+            // Iterate over each color channel for each pixel in image
+            for (size_t ch = 0; ch < num_channels; ++ch) {
+                input_data[ch * nPixels + p] = texture.data[p * num_channels + ch];
+            }
+        }
 
         // Perform inference
         infer_request.Infer();
 
-        ProcessOutput();
+        // locked memory holder should be alive all time while access to its buffer happens
+        LockedMemory<const void> lmoHolder = moutput->rmap();
+        const auto output_data = lmoHolder.as<const PrecisionTrait<Precision::FP32>::value_type*>();
+
+        // Iterate through each pixel in the model output
+        for (size_t p = 0; p < nPixels; p++) {
+            // Iterate through each color channel for each pixel in image
+            for (size_t ch = 0; ch < num_channels; ++ch) {
+                // Get values from the model output
+                data_img[p * num_channels + ch] = static_cast<float>(output_data[ch * nPixels + p]);
+
+                // Clamp color values to the range [0, 255]
+                if (data_img[p * num_channels + ch] < 0) data_img[p * num_channels + ch] = 0;
+                if (data_img[p * num_channels + ch] > 255) data_img[p * num_channels + ch] = 255;
+
+                // Copy the processed output to the OpenCV Mat
+                texture.data[p * num_channels + ch] = data_img[p * num_channels + ch];
+            }
+        }
 
         // Add alpha channel
         cv::cvtColor(texture, texture, cv::COLOR_RGB2RGBA);
         // Copy values form the OpenCV Mat back to inputData
-        std::memcpy(inputData, texture.data, texture.total() * 4);
+        std::memcpy(inputData, texture.data, texture.total() * texture.channels());
     }
 }
